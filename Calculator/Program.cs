cabcd7a4dd245626bc042ad136adced91ac87760
DasD1am0nd <olivia.mock.spam@gmail.com>
2024-04-04 09:44:07 +0200

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class CCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ TestCalculator/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calculator;
+using NUnit.Framework;
+
+namespace TestCalculator
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            CCalculator c1 = new CCalculator();
+            Assert.True(c1.Add(4,5)==9);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
